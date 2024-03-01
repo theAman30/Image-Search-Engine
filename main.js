db409f7f8f0bdf0abd6a10f0819422c3ec915f93
theAman30 <a.srivastav1005@gmail.com>
2024-03-01 14:51:59 +0530

    upgraded

--- main.js
+++ main.js
@@ -9,39 +9,37 @@ let keyword = "";
 let page = 1;
 
 async function searchImages() {
-    keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-    if(page ===1) {
-        searchResult.innerHTML = "";
-    }
-    // console.log(data);
-    const results = data.results;
+  if (page === 1) {
+    searchResult.innerHTML = "";
+  }
+  const results = data.results;
 
-    results.map((result) => {
-        const image = document.createElement("img");
-        image.src = result.urls.small;
-        const imageLink = document.createElement("a");
-        imageLink.href = result.links.html;
-        imageLink.target = "_blank";
-        imageLink.appendChild(image);
-        searchResult.appendChild(imageLink);
-    })
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
 
-    showMoreBtn.style.display = "block";
-
+  showMoreBtn.style.display = "block";
 }
 
-searchForm.addEventListener("submit", (evt) =>{
-    evt.preventDefault();
-    page = 1;
-    searchImages();
-})
+searchForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  page = 1;
+  searchImages();
+});
 
 showMoreBtn.addEventListener("click", () => {
-    page++;
-    searchImages();
-})
+  page++;
+  searchImages();
+});
